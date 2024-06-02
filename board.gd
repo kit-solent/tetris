fd3be31ebe69975bd89f18210dbcaf8798d3d2f3
Shrike <nelsonmaxlang@gmail.com>
2024-06-02 17:50:50 +1200

    SaveBot commit at 2024-06-02 17:50:50

--- board.gd
+++ board.gd
@@ -8,10 +8,12 @@ func _ready():
 	setup_board()
 
 func setup_board():
-	for w in range(width):
-		for h in range(height):
-			set_cell(0,Vector2i(w,h),)
+	clear()
+	# set up the background
+	for w in range(width+2):
+		for h in range(height+2):
+			set_cell(0,Vector2i(w,h),(((h%2)+w)%2)+1)
+			print(tile_set.get_source((((h%2)+w)%2)+1).resource_name)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
