861736a711f5380de144f4da6b04b85ad3f946c3
Shrike <nelsonmaxlang@gmail.com>
2024-06-02 14:29:53 +1200

    SaveBot commit at 2024-06-02 14:29:52

--- board.gd
+++ board.gd
@@ -1,11 +1,16 @@
 extends TileMap
-@export var acceleration=0
-@export var speed
+@export var acceleration:int=0
+@export var speed:int=1
+@export var width:int=10
+@export var height:int=24
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	setup_board()
 
+func setup_board():
+	for w in range(width):
+		for h in range(height):
+			set_cell(0,Vector2i(w,h),)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
